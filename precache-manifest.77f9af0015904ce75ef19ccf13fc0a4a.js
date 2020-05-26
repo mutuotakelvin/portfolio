1f03db0410ae8de81683f9d8e224ce32afd8f896
Mwangi-kelvin3760 <kelvinmutuota749@gmail.com>
2020-05-26 14:39:02 +0300

    Updates

---
+++ precache-manifest.77f9af0015904ce75ef19ccf13fc0a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cda5f86acee3964909c79bc942a6be9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "40a395bce9332f1f45f5",
+    "url": "/portfolio/static/css/main.bdc5e42a.chunk.css"
+  },
+  {
+    "revision": "009e65ad0ec808a7bf80",
+    "url": "/portfolio/static/js/2.0a90521e.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/portfolio/static/js/2.0a90521e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a395bce9332f1f45f5",
+    "url": "/portfolio/static/js/main.d8dc812b.chunk.js"
+  },
+  {
+    "revision": "83f657771b7854dba4c6",
+    "url": "/portfolio/static/js/runtime-main.911f39c4.js"
+  },
+  {
+    "revision": "990daa15334ab8f6f3add4dc71c435d2",
+    "url": "/portfolio/static/media/Kel.990daa15.JPG"
+  }
+]);
